2f48c58473ef6174deff3cfcafb3eaafeae326d6
jeanfe <maestratijf@gmail.com>
2024-02-02 20:27:53 +0100

    move

--- affiche.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-#include <opencv2/core/core.hpp>
-
-
-void afficher_patch(const cv::Mat& patch, const cv::Mat& depths, const cv::Mat& im, const float opacity);
-void debugPatch(const cv::Mat& patchsource, const cv::Mat& patchtarget, const cv::Mat& imsource, const cv::Mat& imtarget, double scaleFactor,bool color);
--- normals.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-#include <opencv2/core/core.hpp>
-#include "normals.hpp"
-
-void normalsIntegration(cv::Mat& normalmap, cv::Mat& depthmap){
-    return;
-}
